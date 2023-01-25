bc925202100c41ec3a3db6d97ca983af1e8c6b5a
Nikhil Sethi <nikhil@Nikhils-MacBook-Pro.local>
2023-01-25 14:32:55 -0800

    shitty v1 button

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const toggleRow = (element) => {
-            element.getElementsByClassName("expanded-row-content").item(0).classList.toggle("hide-row");
-         }
+// Get the button:
+
+window.onload = init;
+var mybutton;
+
+function init(){
+
+    mybutton = document.getElementById("myBtn");
+
+    console.log(document.getElementsByClassName("header"));
+
+    console.log(mybutton);
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+
+}
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
